d5ce187d86bf72c824ecfca2316366f5c1541d1e
AlexArchanfel <lentao_07@mail.ru>
2022-10-14 01:46:35 +0300

    Roma 23

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до n
+void Roma(int x)
+{
+    for (int i = 1; i <= x; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}");
+        if (i != x)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+Console.Clear();
+Console.Write("Введите число: ");  
+int x = int.Parse(Console.ReadLine()!);
+Roma(x);
